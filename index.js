caccb424489af99c781419296fbb834e9acf528f
Kenny Lam <kmlam@ucdavis.edu>
2018-04-09 14:13:10 -0700

    Final ThinkfulTube

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+
+
 function retrieveYoutubeData(searchTerm, callback){
     const query = {
     part: 'snippet',
     key: 'AIzaSyAJmE7PnY-JeE5bWuJcBXQdCxWPZklc8lM',
-    q: `${searchTerm} in:name`,  
-    per_page: 5 
+    q: `${searchTerm}`,  
+    type: 'video',
+    maxResults: 5 
     };
     $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
@@ -14,13 +17,16 @@ function renderData(result) {
     return `
     <div>
         <h2>
-        <a class ="js-result-name" href = "${result.html_url}" target = "_blank"></a>
+        <a href = "https://www.youtube.com/watch?v=${result.id.videoId}" target = "_blank">
+        <img src = "${result.snippet.thumbnails.medium.url}"></a>
         </h2>
         `;
 }
 
 function displayYoutubeSearchData(data){
-        const results = data.items.map((item, index) => renderData(item));
+    console.log(data);
+    const results = data.items.map((item) => renderData(item));
+       
         $('.js-search-results').html(results);
 }
 
@@ -29,8 +35,10 @@ function listenSubmit(){
         event.preventDefault();
         const queryTarget =$(event.currentTarget).find ('.js-query');
         const query = queryTarget.val();
-        queryTarget.val;("");
+        console.log(query);
+        queryTarget.val("");
         retrieveYoutubeData(query, displayYoutubeSearchData);
+        
     })
     console.log('This is listenSubmit');
 }
